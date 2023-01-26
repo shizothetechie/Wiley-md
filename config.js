955d4ae05f2fe83639aca196dd8c9908befd233a
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2023-01-26 16:34:57 +0530

    Wiley-MD ⚡

--- config.js
+++ config.js
@@ -3,30 +3,21 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 
+//Make Changes According to Your Need
+global.bname = 'Wiley-md'     //Your Bot Name
+global.oname = 'Gariox & Shizo'     //Your Name
+global.iglink = 'https://Instagram.com/shizo_the_tech'     //Your Instagram Link
+global.ytlink = 'https://YouTube.com/@errormodsyt      //Your YouTube Channel Link
+global.stkpack = 'Wiley-MD'      //Here name tha your want on sticker
+
+
+
 global.owner = [
   ['919172389527', 'Shizo The Techie', true],
   ['916207142994', 'Gariox 3D', true]
 ] 
 global.prems = []
 
-global.APIs = { 
-  violetics : 'https://violetics.pw',
-  nrtm: 'https://nurutomo.herokuapp.com',
-  bg: 'http://bochil.ddns.net',
-  xteam: 'https://api.xteam.xyz',
-  zahir: 'https://zahirr-web.herokuapp.com',
-  zeks: 'https://api.zeks.xyz',
-  pencarikode: 'https://pencarikode.xyz',
-  LeysCoder: 'https://leyscoders-api.herokuapp.com'
-}
-global.APIKeys = { 
-  'https://violetics.pw': 'beta',
-  'https://api.xteam.xyz': 'd90a9e986e18778b',
-  'https://zahirr-web.herokuapp.com': 'zahirgans',
-  'https://api.zeks.xyz': 'apivinz',
-  'https://pencarikode.xyz': 'pais',
-  'https://leyscoders-api.herokuapp.com': 'dappakntlll'
-}
 
 // Sticker WM
 global.packname = 'TE DEVELOPERS'
