dc2e77d7fd493d4de6a3272fb5fb5005f2b66217
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2023-04-15 10:01:43 +0530

    Upgrade config.js to V2

--- config.js
+++ config.js
@@ -1,35 +1,55 @@
+//Upgraded To Wiley-md V02 
 import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
+import fs from 'fs'
 
-
-//Make Changes According to Your Need
-global.bname = 'Wiley-md'     //Your Bot Name
-global.oname = 'Gariox & Shizo'     //Your Name
-global.iglink = 'https://Instagram.com/shizo_the_tech'     //Your Instagram Link
-global.ytlink = 'https://YouTube.com/@errormodsyt'      //Your YouTube Channel Link
-global.stkpack = 'Wiley-MD'      //Here name tha your want on sticker
-
-
-
+//Important & must be change #ATY
 global.owner = [
-  ['919172389527', 'Shizo The Techie', true],
-  ['916207142994', 'Gariox 3D', true]
+  ['916207142994', 'Gariox 3D', true],
+    ['919172389527', 'Shizo The Techie', true]
 ] 
 global.prems = []
 
+
+//Make Changes #ATY
+global.bname = 'Wiley-md'     //Your Bot Name
+global.oname = 'Gariox & Shizo'     //Your Name
+global.iglink = 'https://Instagram.com/error.mods'     //Your Instagram Link
+global.ytlink = 'https://YouTube.com/@errormodsyt'      //Your YouTube Channel Link
+global.gclink = 'https://chat.whatsapp.com/F8QOoaz3qWqJlI2mwpa1QH' //Your WhatsApp Group Link
+global.script = 'https://github.com/Shizothetechie/wiley-md'  //Donot Change if you appreciate our work 🙂❤️
+
+//ApiKeys (Leave as it is if dont wana face any bug)
+global.openaiKey = ' Your Key Here guys'
+global.shizokeys = 'shizo' //do not change
+global.shizoApikeys = 'shizo' //do not change
+
+//Global Texts
+global.maker = 'Made with ❤️ by TE Developers'
 
 // Sticker WM
 global.packname = 'TE DEVELOPERS'
 global.author = 'Wiley-md'
 
+//Copyrights #MATY
 global.copyright = 'TE DEVELOPERS'
 global.packname = 'Wiley-md'
 global.author = 'TE DEVELOPERS'
 
-//sticker WM
+//sticker WM (change accordingly)
 global.stkpack = 'Wiley-md'
 global.stkowner = 'TE DEVELOPERS'
+
+//Global ImgRes #NMTM
+global.menuimg = fs.readFileSync('./media/menu.png')
+global.btnimg = fs.readFileSync('./media/logo.png')
+global.imgmenu = fs.readFileSync('./media/logo.png')
+
+//Wiley-MD Internal #NMTM
+global.version = '2.0.1'
+global.package = 'com.tedevs.wiley'
+global.library = 'Baileys-MD'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
