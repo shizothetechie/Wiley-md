45bc2cd5048e680bce2aa6f5e91e4379cd4b0746
Shizo The Techie <111331099+dev-shizo@users.noreply.github.com>
2023-04-16 09:01:19 +0530

    Update apikeys.js

--- settings/apikeys.js
+++ settings/apikeys.js
@@ -1,5 +1,5 @@
 
-global.openaiKey = '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'
+global.openaiKey = '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' //add your openai key here this key is disabled
 global.aiKey = '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'
 global.shizokeys = 'shizo' //do not change
 global.shizoApikeys = 'shizo' //do not change
