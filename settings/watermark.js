fa8aa763f8b399003ef336b60d8daf8bed3cdedf
Shizo The Techie <98589136+shizothetechie@users.noreply.github.com>
2023-04-16 09:32:22 +0530

    Watermarks Added

---
+++ settings/watermark.js
@@ -0,0 +1,16 @@
+
+//Global Texts
+global.maker = 'Made with ❤️ by TE Developers'
+
+// Sticker WM
+global.packname = 'TE DEVELOPERS'
+global.author = 'Wiley-md'
+
+//Copyrights #MATY
+global.copyright = 'TE DEVELOPERS'
+global.packname = 'Wiley-md'
+global.author = 'TE DEVELOPERS'
+
+//sticker WM (change accordingly)
+global.stkpack = 'Wiley-md'
+global.stkowner = 'TE DEVELOPERS'
