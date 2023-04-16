2534e5971b6087271fcc5a8e12c9443ffa30aa02
Shizo The Techie <111331099+dev-shizo@users.noreply.github.com>
2023-04-16 08:43:53 +0530

    Separate settings-bot

---
+++ settings/apikeys.js
@@ -0,0 +1,5 @@
+
+global.openaiKey = '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'
+global.aiKey = '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'
+global.shizokeys = 'shizo' //do not change
+global.shizoApikeys = 'shizo' //do not change
